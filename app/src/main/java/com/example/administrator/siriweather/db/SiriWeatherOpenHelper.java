b4482bfa9d2b4cf6fabb653cf9c27a253687d7fc
Low_yong <low_yong@outlook.com>
2016-11-13 11:12:48 +0800

    新增数据库帮忙类，以及各表对应的实体类。

--- app/src/main/java/com/example/administrator/siriweather/db/SiriWeatherOpenHelper.java
+++ app/src/main/java/com/example/administrator/siriweather/db/SiriWeatherOpenHelper.java
@@ -1,5 +1,7 @@
 package com.example.administrator.siriweather.db;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import static android.R.attr.id;
@@ -11,15 +13,25 @@ import static android.R.id.primary;
  */
 
 public class SiriWeatherOpenHelper extends SQLiteOpenHelper{
-    Province:
-    create table Province(
-            id integer primary key autoincrement,
-            province_name text,
-            province_code text)
-        public static final String CREATE_PROVINCE ="create table Province ("
-                + "id integer primary key autoincrement, "
-                + "province_name text, "
-                + "province_code text)";
 
+        //Province表建表语句
+        public static final String CREATE_PROVINCE ="create table Province (" + "id integer primary key autoincrement, " + "province_name text, " + "province_code text)";
 
+        //City表建表语句
+        public static final String CREATE_CITY = "create table City("+"id integer primary key autoincrement, " + "city_name text, " + "city_code text,"+"province_id integer)";
+
+        //County表建表语句
+        public static final String CREATE_COUNTY = "create table City(\"+\"id integer primary key autoincrement, " + "county_name text, " + "county_code text,"+"city_id integer)";
+
+        public SiriWeatherOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory,int version){
+            super(context,name,factory,version);
+        }
+
+        public void onCreate(SQLiteDatabase db){
+            db.execSQL(CREATE_PROVINCE); //创建Province表
+            db.execSQL(CREATE_CITY);  //创建City表
+            db.execSQL(CREATE_COUNTY);  //创建County表
+        }
+
+        public void onUpgrade(SQLiteDatabase db,int oldVersion,int newVersion){}
 }
